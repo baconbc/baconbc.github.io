a1de4470c05cd8ae61f2ff4fd069a47419c2f5c5
baconbc <baconbcr@yahoo.com>
2024-04-05 00:34:27 -0700

    made skills and portfolio cards resize for mobile better and be centered

--- Scripts/gameplay-script.js
+++ Scripts/gameplay-script.js
@@ -1,6 +1,6 @@
 // Function to create gameplay display from template with provided data
-function createGameplay(ID, imgURL, imgAlt, description, bgcolor) {
-    const templateContent = document.getElementById('new-template-content').content;
+function createGameplay(ID, imgURL, imgAlt, description) {
+    const templateContent = document.getElementById('gameplay-template-content').content;
 
     // Clone the template content
     const clone = templateContent.cloneNode(true);
@@ -13,22 +13,18 @@ function createGameplay(ID, imgURL, imgAlt, description, bgcolor) {
     cardImg.src = imgURL;
     cardImg.alt = imgAlt;
 
-    // change the background color of the half w the image so that background is similar color to image if resized
-    const imgBG = clone.querySelector('.img-bg');
-    imgBG.style.backgroundColor = bgcolor;
-
     // Append the cloned card to the container
     document.getElementById(ID).appendChild(clone);
 }
 
 // UI/UX Section
-createGameplay('uiux-container', "Assets/CraftingMechanic.gif", "Inventory and Crafting System", "[Describe Inventory and Crafting System mechanic]", "navy");
-createGameplay('uiux-container', "Assets/Notes.gif", "Note Interface", "[Describe Note mechanic]", "peru");
-createGameplay('uiux-container', "Assets/Hero'sParry.gif", "Main Menu", "[Describe Main Menu]", "gray");
-createGameplay('uiux-container', "Assets/MissedHarvest.gif", "Settings Menu", "[Describe Settings Menu]", "indianred");
+createGameplay('uiux-container', "Assets/CraftingMechanic.gif", "Inventory and Crafting System", "[Describe Inventory and Crafting System mechanic]");
+createGameplay('uiux-container', "Assets/Notes.gif", "Note Interface", "[Describe Note mechanic]");
+createGameplay('uiux-container', "Assets/Hero'sParry.gif", "Main Menu", "[Describe Main Menu]");
+createGameplay('uiux-container', "Assets/MissedHarvest.gif", "Settings Menu", "[Describe Settings Menu]");
 
 // Gameplay Section
-createGameplay('gameplay-container', "Assets/BossMechanics.gif", "Boss fighting mechanics", "This clip showcases the boss Treevil from my latest game Island Boy. I programmed the boss attacks, created the sprites, and worked on the boss summoning mechanic.", "green");
-createGameplay('gameplay-container', "Assets/Fishing.gif", "Fishing mechanic", "[Describe fishing mechanic]", "teal");
-createGameplay('gameplay-container', "Assets/PlantGrowthMechanic.gif", "Plant growing mechanics", "[Describe Plant growing mechanic]", "green");
-createGameplay('gameplay-container', "Assets/MoneyGun.gif", "Money gun mechanic", "[Describe Money Gun mechanic]", "navy");
+createGameplay('gameplay-container', "Assets/BossMechanics.gif", "Boss fighting mechanics", "This clip showcases the boss Treevil from my latest game Island Boy. I programmed the boss attacks, created the sprites, and worked on the boss summoning mechanic.");
+createGameplay('gameplay-container', "Assets/Fishing.gif", "Fishing mechanic", "[Describe fishing mechanic]");
+createGameplay('gameplay-container', "Assets/PlantGrowthMechanic.gif", "Plant growing mechanics", "[Describe Plant growing mechanic]");
+createGameplay('gameplay-container', "Assets/MoneyGun.gif", "Money gun mechanic", "[Describe Money Gun mechanic]");
