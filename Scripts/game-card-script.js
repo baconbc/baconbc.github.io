d79d02c757b18f87b80ce1d719d7ce5d6542998c
baconbc <baconbcr@yahoo.com>
2024-04-14 17:42:02 -0700

    new font, made all important colors used variables, added descriptions to gameplay

--- Scripts/game-card-script.js
+++ Scripts/game-card-script.js
@@ -23,7 +23,14 @@ function createCard(title, imgURL, imgAlt, link, year, info, description) {
 }
 
 // Create instances of the card with different data
-createCard("CraftAway", "Assets/CraftAway.png", "CraftAway video game", "https://jalagon.itch.io/craftaway", "2024", "Unity | Programmer | UI/UX | Pixel Artist", "CraftAway description");
-createCard("AcornQuest", "Assets/AcornQuest.png", "AcornQuest video game", "https://madjuse.itch.io/spring-scaffold-2022", "2022", "Unity | Team Lead | Programmer | UI/UX", "AcornQuest is a 2D platformer game where players try to complete levels through solving puzzles, collecting materials to craft items, and completing jumping challenges, all while evading enemies!");
-createCard("Distant Treasures", "Assets/DistantTreasures.png", "Distant Treasures video game", "https://quetzde6th.itch.io/distant-treasures", "2022", "Unity | Programmer | UI/UX", "Distant Treasures is a 2D story-driven adventure game where players wake up in a mysterious room where they must interact with the environment and solve puzzles to try and escape.");
-createCard("Missed Harvest", "Assets/MissedHarvest.png", "Missed Harvest video game", "https://itch.io/queue/c/2558455/my-games?game_id=2119927", "2023", "Unity | Programmer | UI/UX", "Missed Harvest is a 3D co-op 2-player platformer game where players play as a chicken trying to escape the farm by using items, solving puzzles, and shooting enemies with an egg gun!");
+createCard("CraftAway", "Assets/CraftAway.png", "CraftAway video game", "https://jalagon.itch.io/craftaway", "2024", "Unity | Programmer | UI/UX | Pixel Artist",
+    "CraftAway is a 2D sandbox survival-craft game where players explore a new world and try to survive through gathering resources, crafting items and tools, building a shelter, and fighting enemies.");
+
+createCard("AcornQuest", "Assets/AcornQuest.png", "AcornQuest video game", "https://madjuse.itch.io/spring-scaffold-2022", "2022", "Unity | Team Lead | Programmer | UI/UX",
+    "AcornQuest is a 2D platformer game where players try to complete levels through solving puzzles, collecting materials to craft items, and completing jumping challenges, all while evading enemies!");
+
+createCard("Distant Treasures", "Assets/DistantTreasures.png", "Distant Treasures video game", "https://quetzde6th.itch.io/distant-treasures", "2022", "Unity | Programmer | UI/UX",
+    "Distant Treasures is a 2D story-driven adventure game where players wake up in a mysterious room where they must interact with the environment and solve puzzles to try and escape.");
+
+createCard("Missed Harvest", "Assets/MissedHarvest.png", "Missed Harvest video game", "https://itch.io/queue/c/2558455/my-games?game_id=2119927", "2023", "Unity | Programmer | UI/UX",
+    "Missed Harvest is a 3D co-op 2-player platformer game where players play as a chicken trying to escape the farm by using items, solving puzzles, and shooting enemies with an egg gun!");
--- Scripts/gameplay-script.js
+++ Scripts/gameplay-script.js
@@ -1,12 +1,14 @@
 // Function to create gameplay display from template with provided data
-function createGameplay(ID, imgURL, imgAlt, description) {
+function createGameplay(ID, imgURL, imgAlt, title, description, description2) {
     const templateContent = document.getElementById('gameplay-template-content').content;
 
     // Clone the template content
     const clone = templateContent.cloneNode(true);
 
     // Fill in the placeholders with provided data
-    clone.querySelector('p').textContent = description;
+    clone.querySelector('b').textContent = title;
+    //clone.querySelector('.d1').textContent = description;
+    clone.querySelector('.d2').textContent = description2;
 
     // Modify the image source
     const cardImg = clone.querySelector('.gameplay-img');
@@ -18,13 +20,36 @@ function createGameplay(ID, imgURL, imgAlt, description) {
 }
 
 // UI/UX Section
-createGameplay('uiux-container', "Assets/CraftingMechanic.gif", "Inventory and Crafting System", "[Describe Inventory and Crafting System mechanic]");
-createGameplay('uiux-container', "Assets/Notes.gif", "Note Interface", "[Describe Note mechanic]");
-createGameplay('uiux-container', "Assets/Hero'sParry.gif", "Main Menu", "[Describe Main Menu]");
-createGameplay('uiux-container', "Assets/MissedHarvest.gif", "Settings Menu", "[Describe Settings Menu]");
+createGameplay('uiux-container', "Assets/CraftingMechanic.gif", "Inventory and Crafting System", "Inventory and Crafting System",
+    "[Describe Inventory and Crafting System mechanic]",
+    "Whenever the player picks up an acorn, it adds an item to the inventory. In the inventory, the player can combine items to craft a new item if a valid item combination is used. The player can test out item combinations on their own, or open up the recipe book in order to see all the valid item combinations.");
+
+createGameplay('uiux-container', "Assets/Notes.gif", "Note Interface", "Inventory and Note Interface",
+    "[Describe Note mechanic]",
+    "Whenever the player picks up an item or note, it gets added to the inventory. In the inventory, the player can click on interactable items, like notes. When a note is clicked on, a note reading interface appears to allow the player to read the note.");
+
+createGameplay('uiux-container', "Assets/Hero'sParry.gif", "Main Menu", "Main Menu",
+    "[Describe Main Menu]",
+    "A simple main menu where the player can start the game or quit. I designed all the visuals of the main menu and made it so that the opacity changes when hovering over one of the buttons. When a button is clicked on, the colors change to indicate that it has been pressed, and this color scheme mimics the parry mechanic in the game.");
+
+createGameplay('uiux-container', "Assets/MissedHarvest.gif", "Settings Menu", "Settings Menu",
+    "[Describe Settings Menu]",
+    "In the settings menu, the player can view a brief tutorial, change/mute the volume of sound effects and background music, change the current background music that is playing, or quit to the main menu.");
+
 
 // Gameplay Section
-createGameplay('gameplay-container', "Assets/BossMechanics.gif", "Boss fighting mechanics", "This clip showcases the boss Treevil from my latest game Island Boy. I programmed the boss attacks, created the sprites, and worked on the boss summoning mechanic.");
-createGameplay('gameplay-container', "Assets/Fishing.gif", "Fishing mechanic", "[Describe fishing mechanic]");
-createGameplay('gameplay-container', "Assets/PlantGrowthMechanic.gif", "Plant growing mechanics", "[Describe Plant growing mechanic]");
-createGameplay('gameplay-container', "Assets/MoneyGun.gif", "Money gun mechanic", "[Describe Money Gun mechanic]");
+createGameplay('gameplay-container', "Assets/Fishing.gif", "Fishing mechanic", "Fishing System",
+    "This clip showcases the fishing system that I implemented in my latest game CraftAway.",
+    "After casting the fishing hook out, a fish will appear on the line, indicated by bubbles and a sound. The player must reel in the fishing hook within a couple seconds or else the fish will escape. If the player reeled within the timefame, the fishing minigame will begin. The player must keep the green progress bar on the fish in order to catch it. The fish on the line is randomly selected from the fish database and each fish has varying difficulty and behavior.");
+
+createGameplay('gameplay-container', "Assets/BossMechanics.gif", "Boss fighting mechanics", "Boss Fighting Mechanics",
+    "This clip showcases the boss Treevil from my latest game CraftAway.",
+    "I programmed the boss attacks, created the sprites, and worked on the boss summoning mechanic.");
+
+createGameplay('gameplay-container', "Assets/PlantGrowthMechanic.gif", "Plant growing mechanics", "Plant Growth System",
+    "This clip showcases the plant growth system from my game Farm Game.",
+    "After a seed is planted, the plant goes through several growth stages. During each stage, the plant will indicate when it needs to be watered. The player must water the plant within a certain timeframe or else the plant will wither. Once the plant is fully grown, the plant will indicate that it is ready to be harvested.");
+
+createGameplay('gameplay-container', "Assets/MoneyGun.gif", "Money gun mechanic", "Money Gun Mechanic and Physics",
+    "This clip showcases the money gun mechanic from my game Materialism Attacks!",
+    "Whenever the player clicks, they fire money out of their money gun. The money shoots out fast from the gun for a certain distance, then slowly descends to the ground in a wavy motion to resemble paper falling. Whenever an enemy is hit by money, their 'price tag' (health) reduces by $5. Once the price reaches $0, the enemy dies and a money explosion effect appears.");
